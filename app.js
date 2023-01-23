9af6aaa4fbe6780db5df8c02dded220e6588470e
Pajorad Pajorad <pavle.maodus2004@gmail.com>
2023-01-23 03:24:31 +0100

    website fix

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ tl.to('.text', {y:"0%", duration:1,stagger:0.25})
 tl.to('.slider', {y:"-100%", duration:1.5, delay:0.75})
 tl.to('.intro ', {y:"-100%", duration:1}, "-=1")
 tl.from(".home #bg", {scale: 1.2, ease: 'power4.in',duration:0.6},'=-0.5')
+tl.from(".hero #bg", {scale: 1.2, ease: 'power4.in',duration:0.6},'=-0.5')
 tl.fromTo('nav', {opacity:0}, {opacity:1, duration:1})
 tl.fromTo('.home-text', {opacity:0}, {opacity:1, duration:1},"=-1")
+tl.fromTo('.hero-text', {opacity:0}, {opacity:1, duration:1},"=-1")
 // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 //       (function(){
 //       var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
